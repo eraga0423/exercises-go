33bb34252762760611881ffb94f893d6ec8f9fa4
ydoskeno <doskenovtar@gmail.com>
2025-03-18 17:01:21 +0500

    sdf

--- exercise10/internal/governor/board/list_board.go
+++ exercise10/internal/governor/board/list_board.go
@@ -10,18 +10,54 @@ func (r *LeaderBoard) ListPlayers(req controller.ListPlayerReq) (controller.List
 	if err != nil {
 		return nil, err
 	}
-	newresp := response.GetList()
+	// newresp := response.GetList()
 
-	return newresp, nil
+	// return newresp, nil
+	ps := make([]*Player, 0, len(response.GetList()))
+
+	for _, it := range response.GetList() {
+		ps = append(ps, newPlayer(it.GetPlayerID(), it.GetPlayerName(), it.GetPlayerScore()))
+	}
+
+	return newListPlayer(ps), nil
+
 }
 
 type ListPlayerResp struct {
-	Players []controller.ItemPlayerResp
+	Players []*Player
 }
 
-func newListPlayer() *ListPlayerResp {
-	return &ListPlayerResp{}
+func newListPlayer(ps []*Player) *ListPlayerResp {
+	return &ListPlayerResp{ps}
 }
 func (i *ListPlayerResp) GetList() []controller.ItemPlayerResp {
-	return i.Players
+	list := make([]controller.ItemPlayerResp, 0, len(i.Players))
+
+	for _, p := range i.Players {
+		list = append(list, p)
+	}
+
+	return list
+}
+
+type Player struct {
+	id    string
+	name  string
+	score int
+}
+
+func newPlayer(id, name string, score int) *Player {
+	return &Player{id, name, score}
+}
+
+func (p *Player) GetID() string {
+	return p.id
+}
+
+func (p *Player) GetName() string {
+	return p.name
+}
+
+func (p *Player) GetScore() int {
+	return p.score
 }
